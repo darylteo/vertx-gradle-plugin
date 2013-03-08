757765b498f23ac7e66b786ba90026d664b81d7f
Daryl Teo <i.am@darylteo.com>
2013-03-09 02:26:47 +1100

    Configure maven settings

--- src/main/groovy/com/darylteo/gradle/PropertiesLoader.groovy
+++ src/main/groovy/com/darylteo/gradle/PropertiesLoader.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.vertx.gradle
+package com.darylteo.gradle
 
 import org.gradle.api.*
 
