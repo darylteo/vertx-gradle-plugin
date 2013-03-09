cb25053b00211388b07447e2b0adaaaf2c4cd712
Daryl Teo <i.am@darylteo.com>
2013-03-09 14:00:08 +1100

    Change group name for dep

--- src/main/groovy/com/darylteo/gradle/MavenSettings.groovy
+++ src/main/groovy/com/darylteo/gradle/MavenSettings.groovy
@@ -106,10 +106,6 @@ public class MavenSettings implements Plugin<Project> {
               beforeDeployment { MavenDeployment deployment -> signing.signPom(deployment) }
             }
 
-            pom.project {
-              packaging = 'zip'
-            }
-
             configurePom(pom)
           }
         }
