ec2899fb0863925c75742c16a0abb52e9d95c478
darylteo <i.am@darylteo.com>
2014-05-10 12:48:18 +1000

    [FIX] #18 Config fails with nested json
    TaskInput for GeneratedDeploymentConfig could not be serialized
    causing exception.
    
    Solution - manually serialize the config into a string and use that for serialization
    
    Credit to Ruben Romano <rromano@optionshouse.com> for this bug report.

--- src/main/groovy/com/darylteo/vertx/gradle/tasks/GenerateDeploymentConfig.groovy
+++ src/main/groovy/com/darylteo/vertx/gradle/tasks/GenerateDeploymentConfig.groovy
@@ -1,22 +1,25 @@
 package com.darylteo.vertx.gradle.tasks
 
-import groovy.json.JsonOutput;
-
+import com.darylteo.vertx.gradle.deployments.Deployment
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-import com.darylteo.vertx.gradle.deployments.Deployment
-
-class GenerateDeploymentConfig extends DefaultTask{
+class GenerateDeploymentConfig extends DefaultTask {
 
   Deployment deployment
   def outputFile
 
   public setDeployment(Deployment deployment) {
     this.deployment = deployment
-    
-    this.inputs.property('config', deployment.config)
-    
+
+    this.inputs.property('config', {
+      JsonBuilder json = new JsonBuilder()
+      json.call(deployment.config)
+      return json.toPrettyString()
+    })
+
     this.outputFile = { "${project.buildDir}/configs/${deployment.name}.conf" }
     this.outputs.file outputFile
   }
@@ -30,8 +33,8 @@ class GenerateDeploymentConfig extends DefaultTask{
     def dir = file.parentFile
 
     file.delete()
-    
-    if(dir.isDirectory() || dir.mkdirs()) {
+
+    if (dir.isDirectory() || dir.mkdirs()) {
       file << JsonOutput.toJson(deployment.config)
     } else {
       throw new Exception("Could not create directory: $dir")
